cab7087d5d0b32498db124c6dc9baee0bf5f2212
MarlenaMoon <111521128+MarlenaMoon@users.noreply.github.com>
2023-03-03 18:06:05 -0600

    Updated final

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
 
+import Header from './Header';
+import Nav from './Nav';
+import Footer from './Footer';
+import Main from './Main';
+
+
 function App() {
     return ( <
-        div className = "App" >
+        >
         <
-        h1 > This is my final project < /h1> <
-        /div>
+        body >
+        <
+        Header > zIndex: < /Header> <
+        Nav > zIndex: < /Nav> <
+        /body> <
+        body >
+        <
+        Main > < /Main> <
+        /body> <
+        body >
+        <
+        Footer > zIndex: < /Footer> <
+        /body> <
+        />
     );
 }
 
---
+++ src/Main.js
@@ -0,0 +1,15 @@
+function Main() {
+    return ( <
+        body >
+        <
+        article >
+        <
+        h1 > Little Lemon < /h1> <
+        h2 > Chicago < /h2> <
+        h5 > Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. < /h5> <
+            /article> <
+            /body>
+    );
+};
+
+export default Main;
